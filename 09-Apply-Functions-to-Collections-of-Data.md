a28fb7562880d107af9121ba9d4db78bdca3990d
elfgod <albertogx@icloud.com>
2022-01-17 23:24:30 -0500

    09 Apply Functions to Collections of Data, part4, Filter, Every & Some Methods

--- 09-Apply-Functions-to-Collections-of-Data.md
+++ 09-Apply-Functions-to-Collections-of-Data.md
@@ -109,3 +109,39 @@ return n % 2 === 1;
 
 const smallNums = nums.filter(n => n < 5);
 // [4, 3, 2, 1]
+
+### EVERY
+
+tests whether all elements in the array pass the
+provided function. It returns a Boolean value.
+
+const words = ['dog', 'dig', 'log', 'bag', 'wag'];
+
+words.every(word => {
+return word.length === 3;
+}) // true
+
+words.every(word => word[0] === 'd'); // false
+
+words.every(w => {
+let last_letter = w[w.length - 1];
+return last_letter === 'g'
+}) // true
+
+### SOME
+
+Similar to every, but returns true if ANY of
+the array elements pass the test function
+
+const words = ['dog', 'dig', 'log', 'bag', 'wag'];
+
+// Are there any words longer than 4 characters?
+words.some(word => {
+return word.length > 4;
+}) // true
+
+// Do any words start with 'Z'?
+words.some(word => word[0] === 'Z'); // false
+
+// Do any words contain 'cake?
+words.some(w => w.includes('cake)) // true
--- 09.js
+++ 09.js
@@ -10,6 +10,7 @@ numbers.forEach(function (element) {
 })
 
 console.log('\n');
+
 function printTriple(num) {
   console.log(num * 3);
 }
@@ -19,25 +20,25 @@ numbers.forEach(printTriple);
 console.log('\n');
 
 const books = [{
-  title: 'Good Omens',
-  authors: ['Terry Pratchett', 'Neil Gaiman'],
-  rating: 4.25
-},
-{
-  title: 'Bone: The Complete Edition',
-  authors: ['Jeff Smith'],
-  rating: 4.42
-},
-{
-  title: 'American Gods',
-  authors: ['Neil Gaiman'],
-  rating: 4.11
-},
-{
-  title: 'A Gentleman in Moscow',
-  authors: ['Amor Towles'],
-  rating: 4.36
-}
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36
+  }
 ]
 
 books.forEach(function (book) {
@@ -45,7 +46,7 @@ books.forEach(function (book) {
 })
 
 console.log('\n');
-for(let book of books) {
+for (let book of books) {
   console.log(book.title.toUpperCase());
 }
 
@@ -56,15 +57,15 @@ for (let i = 0; i < books.length; i++) {
 
 console.log('\n');
 // printing the Index of numbers
-numbers.forEach(function(num, index) {
+numbers.forEach(function (num, index) {
   console.log(index, num);
 })
 
 console.log('\n');
 
-const words =['asap', 'byob', 'rsvp', 'diy'];
+const words = ['asap', 'byob', 'rsvp', 'diy'];
 
-const doubles = numbers.map(function(num) {
+const doubles = numbers.map(function (num) {
   return num * 2;
 })
 console.log(doubles);
@@ -81,26 +82,26 @@ console.log(numDetail);
 // numbers = [20, 21, 22, 23, 24, 25, 26, 27];
 console.log('\n');
 const doubles2 = [];
-for(let num of numbers) {
+for (let num of numbers) {
   doubles2.push(num * 2);
 }
 console.log(doubles2);
 
 console.log('\n');
-const abbrevs = words.map(function(word) {
+const abbrevs = words.map(function (word) {
   return word.toUpperCase().split('').join('.');
 })
 console.log(abbrevs);
 
 console.log('\n');
-const titles = books.map(function(b) {
+const titles = books.map(function (b) {
   return b.title;
 })
 console.log(titles);
 
 console.log('\n');
 const square = (x) => {
-  return console.log(`Square of ${x} is: `, x * x) ;
+  return console.log(`Square of ${x} is: `, x * x);
 }
 square(2);
 square(10);
@@ -137,7 +138,7 @@ console.log('\n');
 const squareImplicit = n => (
   console.log(`${n} * ${n}: `, n * n)
 )
-  
+
 squareImplicit(11);
 
 console.log('\n');
@@ -146,7 +147,7 @@ squareImplicit2(5);
 
 console.log('\n');
 const nums = [1, 2, 3, 4, 5, 6, 7, 8];
-const doublesX1 = nums.map(function(n) {
+const doublesX1 = nums.map(function (n) {
   return console.log(`${n} * 2`, n * 2);
 })
 
@@ -156,35 +157,35 @@ const doublesX2 = nums.map(n => {
 })
 
 console.log('\n');
-const doublesX3 = nums.map (n => console.log(`${n} * 2`, n * 2));
+const doublesX3 = nums.map(n => console.log(`${n} * 2`, n * 2));
 
 console.log('\n');
-const parityList = nums.map(function(n) {
-  if(n % 2 === 0) return console.log(`${n} is even!.`)
+const parityList = nums.map(function (n) {
+  if (n % 2 === 0) return console.log(`${n} is even!.`)
   return console.log(`${n} is odd`)
 })
 
 console.log('\n');
 const parityListArrow = nums.map((n) => {
-  if(n % 2 === 0) return console.log(`${n} is even!`);
+  if (n % 2 === 0) return console.log(`${n} is even!`);
   return console.log(`${n} is odd`)
 })
 
 console.log('\n');
 const parityTernary = nums.map((n) => (
-    n % 2 === 0
-    ? console.log('isEven')
-    : console.log('odd')
+  n % 2 === 0 ?
+  console.log('isEven') :
+  console.log('odd')
 ))
 console.log('\n');
 const partityTernary1Line = nums.map(n => n % 2 === 0 ? console.log('isEven') : console.log('odd'))
 
 console.log('\n');
 let movies = [
-"The Fantastic Mr. Fox",
-"Mr. and Mrs. Smith",
-"Mrs. Doubtfire",
-"Mr. Deeds"
+  "The Fantastic Mr. Fox",
+  "Mr. and Mrs. Smith",
+  "Mrs. Doubtfire",
+  "Mr. Deeds"
 ]
 const movie = movies.find(movie => {
   return movie.includes('Mrs');
@@ -219,50 +220,116 @@ console.log(bigNums);
 
 console.log('\n');
 const bookz = [{
-  title: 'Good Omens',
-  authors: ['Terry Pratchett', 'Neil Gaiman'],
-  rating: 4.25,
-  genres: ['fiction', 'fantasy']
-},
-{
-  title: 'Changing My Mind',
-  authors: ['Zadie Smith'],
-  rating: 3.83,
-  genres: ['nonfiction', 'essays']
-}
-{
-  title: 'Bone: The Complete Edition',
-  authors: ['Jeff Smith'],
-  rating: 4.42,
-  genres: ['fiction', 'graphic novel', 'fantasy']
-},
-{
-  title: 'American Gods',
-  authors: ['Neil Gaiman'],
-  rating: 4.11,
-  genres: ['fiction', 'fantasy']
-},
-{
-  title: 'A Gentleman in Moscow',
-  authors: ['Amor Towles'],
-  rating: 4.36,
-  genres: ['fiction', 'historical fiction']
-},
-{
-  title: 'The Name of the Wind',
-  authors: ['Patrick Rothfuss'],
-  rating: 4.54,
-  genres: ['fiction', 'fantasy']
-},
-{
-  title: 'Lord of the flies',
-  authors: ['William Golding'],
-  rating: 3.67,
-  genres: ['fiction']
-}
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'Changing My Mind',
+    authors: ['Zadie Smith'],
+    rating: 3.83,
+    genres: ['nonfiction', 'essays']
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42,
+    genres: ['fiction', 'graphic novel', 'fantasy']
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36,
+    genres: ['fiction', 'historical fiction']
+  },
+  {
+    title: 'The Name of the Wind',
+    authors: ['Patrick Rothfuss'],
+    rating: 4.54,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'The Overstory',
+    authors: ['Richard Powers'],
+    rating: 4.19,
+    genres: ['fiction', 'short stories']
+  },
+  {
+    title: 'The Way of Kings',
+    authors: ['Brandon Sanderson'],
+    rating: 4.65,
+    genres: ['fantasy', 'epic']
+  },
+  {
+    title: 'Lord of the flies',
+    authors: ['William Golding'],
+    rating: 3.67,
+    genres: ['fiction']
+  }
 ]
 
-
+const goodBookz = bookz.filter(b => b.rating > 4.3);
+console.log(goodBookz);
 
 console.log('\n');
+const fantasyBooks = bookz.filter(book => (
+  book.genres.includes('fantasy')
+))
+console.log(fantasyBooks);
+
+console.log('\n');
+const shortForm = bookz.filter(book => (
+  book.genres.includes('short stories') ||
+  book.genres.includes('essays')
+))
+console.log(shortForm);
+
+console.log('\n');
+const query = 'The';
+const results = bookz.filter(book => {
+  return book.title.toLowerCase()
+  .includes(query.toLowerCase())
+})
+console.log(results);
+
+console.log('\n');
+const wordz = ['dog', 'dig', 'log', 'bag', 'wag'];
+console.log("Array wordz: ", wordz);
+console.log('\n');
+// returns only Boolean true or false
+// returns true if every single element is length === 3
+const all3Letters = wordz.every(word => word.length === 3);
+console.log("all3Letters: ", all3Letters);
+
+console.log('\n');
+const allEndInG = wordz.every(word => {
+  const last = word.length-1;
+  return word[last] === 'g';
+})
+console.log("allEndInG: ", allEndInG);
+
+console.log('\n');
+const someStartWithD = wordz.some(word => (
+  word[0] === 'd'
+))
+console.log("someStartWithD: ", someStartWithD);
+
+console.log('\n');
+const allStartWithD = words.every(word => {word[0] == 'd'})
+console.log("allStartWithD: ", allStartWithD);
+
+console.log('\n');
+const allGoodBooks = bookz.every(book => book.rating > 3.5);
+console.log('allGoodBooks: ', allGoodBooks);
+
+console.log('\n');
+const any2Authors = bookz.some(book => book.authors.length === 2);
+console.log('any2Authors: ', any2Authors);
 console.log('\n');
