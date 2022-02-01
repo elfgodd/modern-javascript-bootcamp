85273661464389f1f82aefe03fda13916e58a997
elfgod <albertogx@icloud.com>
2022-01-31 23:06:55 -0500

    13 - Twisting the DOM to Our Will: NBA Scores Chart Pt1

---
+++ 13/nba-scores-chart/app.js
@@ -0,0 +1,125 @@
+const warriorsGames = [{
+  awayTeam: {
+    team: 'Golden State',
+    points: 119,
+    isWinner: true
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 106,
+    isWinner: false
+  }
+},
+{
+  awayTeam: {
+    team: 'Golden State',
+    points: 105,
+    isWinner: false
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 127,
+    isWinner: true
+  }
+},
+{
+  homeTeam: {
+    team: 'Golden State',
+    points: 126,
+    isWinner: true
+  },
+  awayTeam: {
+    team: 'Houston',
+    points: 85,
+    isWinner: false
+  }
+},
+{
+  homeTeam: {
+    team: 'Golden State',
+    points: 92,
+    isWinner: false
+  },
+  awayTeam: {
+    team: 'Houston',
+    points: 95,
+    isWinner: true
+  }
+},
+{
+  awayTeam: {
+    team: 'Golden State',
+    points: 94,
+    isWinner: false
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 98,
+    isWinner: true
+  }
+},
+{
+  homeTeam: {
+    team: 'Golden State',
+    points: 115,
+    isWinner: true
+  },
+  awayTeam: {
+    team: 'Houston',
+    points: 86,
+    isWinner: false
+  }
+},
+{
+  awayTeam: {
+    team: 'Golden State',
+    points: 101,
+    isWinner: true
+  },
+  homeTeam: {
+    team: 'Houston',
+    points: 92,
+    isWinner: false
+  }
+}
+]
+
+const ulParent = document.createElement('ul');
+for(let game of warriorsGames) {
+  const {
+    homeTeam,
+    awayTeam
+  } = game;
+  
+  const gameLi = document.createElement('li');
+
+  const {
+    team: hTeam,
+    points: hPoints
+  } = homeTeam;
+
+  const {
+    team: aTeam,
+    points: aPoints
+  } = awayTeam;
+
+  const teamNames= `${aTeam} @ ${hTeam}`;
+  let scoreLine;
+  if (aPoints > hPoints) {
+    scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+  } else {
+    scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+  }
+  const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+  gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
+
+  console.log(warriors);
+
+
+  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+  // console.log(scoreLine);
+  ulParent.append(gameLi);
+  // console.log(awayTeam.team, homeTeam.team);
+}
+
+document.body.prepend(ulParent);
