8e50045ecefae26bd36171465cde6ec7fbcece80
ElfGod <albertogx@icloud.com>
2022-03-08 22:01:21 -0500

    Updating docs in folder 04

--- 04/04-Capture-Collections-of-Data-with-Arrays.md
+++ 04/04-Capture-Collections-of-Data-with-Arrays.md
@@ -4,81 +4,83 @@
 
 Collections of Data
 
-Work with Arrays
-Write object literals
-Understand reference types
+Work with Arrays  
+Write object literals  
+Understand reference types  
 Use common Array/Object methods
 
 ### ARRAYS
 
-Ordered colllections of values
-List of comments in IG post
-Collection of levels in a game
+Ordered colllections of values  
+List of comments in IG post  
+Collection of levels in a game  
 Songs in a playlist
 
 ### CREATING ARRAYS
 
-// To make an empty array
+// To make an empty array  
 let students = [];
 
-// An array of numbers
+// An array of numbers  
 let lottoNums = [19, 22, 56, 12, 51];
 
-// A mixed array
+// A mixed array  
 let stuff = [true, 68, 'cat', null];
 
-// An array of strings
+// An array of strings  
 let colors = ['red', 'orange', 'yellow', 'green'];
 
 ### ARRAYS ARE INDEXED
 
 colors.length // 4
 
-colors[0] // 'red'
-colors[1] // 'orange'
-colors[2] // 'yellow'
-colors[3] // 'green'
+colors[0] // 'red'  
+colors[1] // 'orange'  
+colors[2] // 'yellow'  
+colors[3] // 'green'  
 colors[4] // 'undefined'
 
 ### MODIFYING ARRAYS
 
-colors[3] = 'black'
-colors[4] // undefined
+colors[3] = 'black'  
+colors[4] // undefined  
 colors[4] = 'blue'
 
 ### ARRAYS METHODS
 
-Push - add to end
-Pop - remove from end
-Shift - remove from start
+Push - add to end  
+Pop - remove from end  
+Shift - remove from start  
 Unshift - add to start
 
 You might mix up shift and unshift for a while
 
 ### MORE METHODS!
 
-concat - merge arrays
-includes - merge arrays
-indexOf - just like str.indexOf
-join - creates a string from arr
-reverse - reverses an array
-slice - copy portion of an arr
-splice - remove/replace elements
+concat - merge arrays  
+includes - merge arrays  
+indexOf - just like str.indexOf  
+join - creates a string from array  
+reverse - reverses an array  
+slice - copy portion of an array  
+splice - remove/replace elements  
 sort - sorts an array
 
 ### NOW
 
-Work with Arrays
-Write Object Literals
-Understand Reference Types
+Work with Arrays  
+Write Object Literals  
+Understand Reference Types  
 Use common Array/Object methods
 
 ### VALUES CAN CHANGE
 
-As long as the reference remains the same
-const myEggs = ['brown', 'brown'];
-myEggs.push('purple);
-myEggs[0] = 'green';
+As long as the reference remains the same  
+const myEggs = ['brown', 'yellow'];  
+myEggs.push('purple);  
+['brown', 'yellow', 'purple']  
+myEggs[0] = 'green';  
+['green', 'brown', 'purple']
 
 myEggs = ['blue', 'pink']; // NO! TypeError
 
@@ -86,11 +88,11 @@ myEggs = ['blue', 'pink']; // NO! TypeError
 
 We can store arrays inside others arrays!
 
-const colors = [
-['red', 'crimson'],
-['orange', 'dark orange'],
-['yellow', 'golden rod'];
-['green', 'olive'];
-['blue', 'navy blue'];
-['purple', 'orchid'];
+const colors = [  
+['red', 'crimson'],  
+['orange', 'dark orange'],  
+['yellow', 'golden rod'];  
+['green', 'olive'];  
+['blue', 'navy blue'];  
+['purple', 'orchid'];  
 ]
--- 04/04.js
+++ 04/04.js
@@ -1,31 +1,26 @@
-let shoppingList = ['cereal', 'cheese','ice'];
+let shoppingList = ['cereal', 'cheese', 'ice']
 
-let lotto = [45, 12, 23, 25, 34];
+let lotto = [45, 12, 23, 25, 34]
 
-let myCollection = [12, 'dog', true, null, NaN];
+let myCollection = [12, 'dog', true, null, NaN]
 
-console.log('lotto:',lotto[123]); // undefined
-console.log('abcdefg:', "abcdefg"[123]) // undefined
+console.log('lotto:', lotto[123]) // undefined
+console.log('abcdefg:', 'abcdefg'[123]) // undefined
 
-console.log(shoppingList);
-shoppingList[0] = 'milk';
-console.log(shoppingList);
-shoppingList[2] = 'ice cream';
-console.log(shoppingList);
+console.log(shoppingList)
+shoppingList[0] = 'milk'
+console.log(shoppingList)
+shoppingList[2] = 'ice cream'
+console.log(shoppingList)
 
-let topSongs = [
-  'Another night',
-  'White soul',
-  'Heart break',
-  'Benz'
-];
+let topSongs = ['Another night', 'White soul', 'Heart break', 'Benz']
 
-console.log(topSongs);
-topSongs.push('Soulfly');
-console.log(topSongs);
+console.log(topSongs)
+topSongs.push('Soulfly')
+console.log(topSongs)
 let rodWave = topSongs.pop()
-console.log(rodWave);
-console.log(topSongs);
+console.log(rodWave)
+console.log(topSongs)
 
 let dishesToDo = ['big platter']
 console.log(dishesToDo)
@@ -46,175 +41,239 @@ dishesToDo.shift()
 console.log(dishesToDo)
 dishesToDo.shift()
 
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
+const array1 = ['a', 'b', 'c']
+const array2 = ['d', 'e', 'f']
 
-console.log(array1.concat(array2));
+console.log(array1.concat(array2))
+// ['a', 'b', 'c', 'd', 'e', 'f']
 
-let fruits = ['apple', 'banana'];
-let veggies = ['asparagus', 'brussel sprouts'];
-let meats = ['steak', 'chicken breast'];
+let fruits = ['apple', 'banana']
+let veggies = ['asparagus', 'brussel sprouts']
+let meats = ['steak', 'chicken breast']
 
-console.log(fruits.concat(veggies));
-console.log(veggies.concat(fruits));
+console.log(fruits.concat(veggies))
+console.log(veggies.concat(fruits))
 
-let allFood = fruits.concat(veggies, meats);
-console.log(allFood);
+let allFood = fruits.concat(veggies, meats)
+console.log(allFood)
 
-let ingredients = ['water', 'corn starch', 'flour', 'cheese',
-'brownsuggar', 'shrimp','eel', 'butter'];
+let ingredients = [
+  'water',
+  'corn starch',
+  'flour',
+  'cheese',
+  'brownsuggar',
+  'shrimp',
+  'eel',
+  'butter',
+]
 
-console.log(ingredients.includes('fish'));
-console.log(ingredients.includes('shrimp'));
-console.log(ingredients.includes('corn'));
+console.log(ingredients.includes('fish')) // false
+console.log(ingredients.includes('shrimp')) // true
+console.log(ingredients.includes('corn')) // false
 
 // fromIndex, where to start searching from
-console.log(ingredients.includes('water', 3));
-console.log(ingredients.includes('water', 0));
+console.log(ingredients.includes('water', 3)) // false
+console.log(ingredients.includes('water', 0)) // true
 
 if (ingredients.includes('flour')) {
-  console.log('I am gluten free, i cannot eat that');
+  console.log('I am gluten free, i cannot eat that')
 }
 
-console.log(ingredients.indexOf('eel'));
-console.log(ingredients.indexOf('maple syrup'));
-console.log(ingredients.indexOf('flour') !== -1 );
+console.log(ingredients.indexOf('eel')) // 6
+console.log(ingredients.indexOf('maple syrup')) // -1
+console.log(ingredients.indexOf('flour') !== -1) // true
 
-let beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
-console.log(beasts.indexOf('bison'));
+let beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+console.log(beasts.indexOf('bison')) // 1
 
 // start from index 2
-console.log(beasts.indexOf('bison', 2));
-console.log(beasts.indexOf('giraffe'));
+console.log(beasts.indexOf('bison', 2)) // 4
+console.log(beasts.indexOf('giraffe')) // -1
 
-let arrayToReverse = ['one', 'two', 'three'];
-console.log('array:', arrayToReverse);
+let arrayToReverse = ['one', 'two', 'three']
+console.log('array:', arrayToReverse)
 
-let arrayReversed = arrayToReverse.reverse();
-console.log(arrayReversed);
+let arrayReversed = arrayToReverse.reverse()
+// ['three', 'two', 'one']
+console.log(arrayReversed)
 
-let elements = ['fire', 'air', 'water'];
+let elements = ['fire', 'air', 'water']
 
-console.log(elements);
-console.log(elements.join());
-console.log(elements.join(''));
-console.log(elements.join('-'));
-console.log(elements.join(', '));
-console.log(elements.reverse().join('_'));
-console.log([12.3, 60, false]);
-console.log([12.3, 60, false].join());
-console.log([12.3, 60, false].join(' # -> '));
+console.log(elements)
+// ['fire', 'air', 'water']
+console.log(elements.join())
+// fire,air,water
+console.log(elements.join(''))
+// fireairwater
+console.log(elements.join('-'))
+// fire-air-water
+console.log(elements.join(', '))
+// fire, air, water
+console.log(elements.reverse().join('_'))
+// water_air_fire
+console.log([12.3, 60, false])
+// [12.3, 60, false]
+console.log([12.3, 60, false].join())
+// 12.3,60,false
+console.log([12.3, 60, false].join(' # -> '))
+// 12.3 # -> 60 # -> false
 
-let animals = ['shark', 'salmon', 'whale', 'bear', 'elephant',
-'lizard', 'tortoise',];
+let animals = [
+  'shark',
+  'salmon',
+  'whale',
+  'bear',
+  'elephant',
+  'lizard',
+  'tortoise',
+]
 
-// creates a new array
-console.log(animals);
-console.log(animals.slice(2));
-console.log(animals.slice(2, 4));
-console.log(animals.slice(1,5));
+// creates a new array with its own memory location
+console.log(animals)
+// ['shark', 'salmon', 'whale', 'bear', 'elephant', 'lizard', 'tortoise']
+console.log(animals.slice(2))
+// ['whale', 'bear', 'elephant', 'lizard', 'tortoise']
+console.log(animals.slice(2, 4))
+// ['whale', 'bear']
+console.log(animals.slice(1, 5))
+// ['salmon', 'whale', 'bear', 'elephant']
 
-let swimmers = animals.slice(0, 3);
-console.log(swimmers);
+let swimmers = animals.slice(0, 3)
+console.log(swimmers)
+// ['shark', 'salmon', 'whale']
 
-let mammals = animals.slice(2, 5);
-console.log(mammals);
+let mammals = animals.slice(2, 5)
+console.log(mammals)
+// ['whale', 'bear', 'elephant']
 
-let reptiles = animals.slice(5, 7);
-let reptiles2 = animals.slice(5);
-console.log(reptiles);
-console.log(reptiles2);
+let reptiles = animals.slice(5, 7)
+// ['lizard', 'tortoise']
+let reptiles2 = animals.slice(5)
+// ['lizard', 'tortoise']
+console.log(reptiles)
+console.log(reptiles2)
 
-let quadruped = animals.slice(-3);
-console.log(quadruped); 
-console.log(animals.slice(-3, -1));
+let quadruped = animals.slice(-3)
+console.log(quadruped)
+// ['elephant', 'lizard', 'tortoise']
+console.log(animals.slice(-3, -1))
+// ['elephant', 'lizard']
 
-let copySlice = animals.slice();
-console.log(copySlice);
+let copySlice = animals.slice()
+console.log(copySlice)
+// ['shark', 'salmon', 'whale', 'bear', 'elephant', 'lizard', 'tortoise']
 
-let months = ['jan', 'march', 'april', 'June'];
-console.log(months);
+let months = ['jan', 'march', 'april', 'june']
+
+console.log(months)
+// ['jan', 'march', 'april', 'june']
 // inserts at index 1
-months.splice(1, 0, 'feb');
-console.log(months);
+months.splice(1, 0, 'feb')
+console.log(months)
+// ['jan', 'feb', 'march', 'april', 'june']
 // replaces 1 element at index 4
-months.splice(4, 1, 'may');
-console.log(months);
+months.splice(4, 1, 'may')
+console.log(months)
+// ['jan', 'feb', 'march', 'april', 'may']
 
-console.log(animals);
-animals.splice(1, 0, 'octopus');
-console.log(animals);
+console.log('########')
+// animals = [
+//   'shark',
+//   'salmon',
+//   'whale',
+//   'bear',
+//   'elephant',
+//   'lizard',
+//   'tortoise',
+// ]
 
-animals.splice(3, 2);
-console.log(animals);
+console.log(animals)
+// ['shark', 'salmon', 'whale', 'bear', 'elephant', 'lizard', 'tortoise']
+animals.splice(1, 0, 'octopus')
+console.log(animals)
+// ['shark', 'octopus', 'salmon', 'whale', 'bear', 'elephant', 'lizard', 'tortoise']
 
-animals.splice(3, 0, 'snake', 'frog');
-console.log(animals);
+animals.splice(3, 2)
+console.log(animals)
+// ['shark', 'octopus', 'salmon', 'elephant', 'lizard', 'tortoise']
 
-animals.splice(0, 2, 'SHARK!', 'OCTOPUS!');
-console.log(animals);
+animals.splice(3, 0, 'snake', 'frog')
+console.log(animals)
+// ['shark', 'octopus', 'salmon', 'snake', 'frog', 'elephant', 'lizard', 'tortoise']
 
-console.log(months);
-months.sort();
-console.log(months);
+animals.splice(0, 2, 'SHARK!', 'OCTOPUS!')
+console.log(animals)
+// ['SHARK!', 'OCTOPUS!', 'salmon', 'snake', 'frog', 'elephant', 'lizard', 'tortoise']
 
-let arraySort = [1, 30, 4, 21, 10000];
-arraySort.sort();
-console.log(arraySort);
+// months = ['jan', 'march', 'april', 'june']
+console.log(months)
+// ['jan', 'march', 'april', 'june']
+months.sort()
+console.log(months)
+// ['april', 'jan', 'june', 'march']
 
-console.log(topSongs);
-console.log(topSongs.sort());
-console.log(topSongs);
+let arraySort = [1, 30, 4, 21, 10000]
+arraySort.sort()
+// [1, 10000, 21, 30, 4]
+console.log(arraySort)
+
+// topSongs = ['Another night', 'White soul', 'Heart break', 'Benz']
+console.log(topSongs)
+console.log(topSongs.sort())
+// ['Another night', 'Benz', 'Heart break', 'White soul']
+console.log(topSongs)
 
 // Reference Types
-let numz = [1, 2, 3, 4, 5];
-console.log(numz);
-let otherNumz = numz;
-console.log(otherNumz);
+let numz = [1, 2, 3, 4, 5]
+console.log(numz)
+let otherNumz = numz
+console.log(otherNumz)
 
-numz.push(6, 7, 8);
-console.log(numz);
-console.log(otherNumz);
-otherNumz.pop();
-otherNumz.pop();
-otherNumz.pop();
-otherNumz.pop();
-console.log(numz);
-console.log(otherNumz);
+numz.push(6, 7, 8)
+console.log(numz)
+console.log(otherNumz)
+otherNumz.pop()
+otherNumz.pop()
+otherNumz.pop()
+otherNumz.pop()
+console.log(numz)
+console.log(otherNumz)
 
 // Using consts with Arrays
-let x = 34;
-x += 5;
-console.log(x);
-const city = 'Lisbon';
-console.log(city);
+let x = 34
+x += 5
+console.log(x)
+const city = 'Lisbon'
+console.log(city)
 // city = 'Londron'; // Uncaught TypeError: invalid assignment to const 'city'
 
-const foods = ['milk'];
-foods.push('chocolate');
-console.log(foods);
-foods.unshift('tortillas');
-console.log(foods);
-foods.pop();
-foods.pop();
-foods.pop();
-console.log(foods);
+const foods = ['milk']
+foods.push('chocolate')
+console.log(foods)
+// ['milk', 'chocolate']
+foods.unshift('tortillas')
+console.log(foods)
+// ['tortillas', 'milk', 'chocolate']
+foods.pop()
+foods.pop()
+foods.pop()
+console.log(foods)
+// []
 // foods = []; // Uncaught TypeError: invalid assignment to const 'foods'
 
-const arrayWhatEverWeWant = [null, undefined, NaN, 'abcd', 123456789];
-console.log(arrayWhatEverWeWant);
-
+const arrayWhatEverWeWant = [null, undefined, NaN, 'abcd', 123456789]
+console.log(arrayWhatEverWeWant)
 
 const animalPairs = [
-  ['doe', 'buck'],
-  ['ewe', 'ram'],
-  ['peahen', 'peacock']
-];
+  ['doe', 'buck'], // 0
+  ['ewe', 'ram'], // 1
+  ['peahen', 'peacock'], // 2
+]
 console.log(animalPairs)
-console.log(animalPairs[2]);
-console.log(animalPairs[2][0]);
-console.log(animalPairs[2][1]);
-console.log(animalPairs[1][1]);
-animalPairs[0][1] = 'stag';
-console.log(animalPairs[0]);
-
+console.log(animalPairs[2]) // ['peahen', 'peacock']
+console.log(animalPairs[2][0]) // 'peahen'
+console.log(animalPairs[2][1]) // 'peacock'
+console.log(animalPairs[1][1]) // 'ram'
+animalPairs[0][1] = 'stag' // 'buck' changes to 'stag'
+console.log(animalPairs[0]) // ['doe', 'buck']
